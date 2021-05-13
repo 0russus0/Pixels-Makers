cf656637607e85b8e470097a0670260ea229e989
Russus <pierreetleroux@gmail.com>
2021-05-13 20:22:34 +0200

    Creation des CRUD

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,9 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Entity\Artwork;
+use App\Entity\Category;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -28,6 +31,8 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('Artworks', 'fas fa-pencil-alt', EntityClass::class);
+        yield MenuItem::linkToCrud('Makers', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Artworks', 'fas fa-pencil-alt', Artwork::class);
+        yield MenuItem::linkToCrud('Catégories', 'fas fa-indent', Category::class);
     }
 }
